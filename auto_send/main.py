0b2863964883028ccac15a744bfe5d8e5efb6e83
fenwickxie <25617200+fenwickxie@users.noreply.github.com>
2025-04-15 18:13:25 +0800

    Update main.py

---
+++ auto_send/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project : officeauto 
+@File    : main.py
+@IDE     : PyCharm 
+@Author  : xie.fangyu
+@Date    : 2025/3/31 下午1:58 
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from gui import WeChatSchedulerUI
+from scheduler import WeChatScheduler
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    
+    # 创建业务逻辑层实例
+    scheduler = WeChatScheduler()
+    
+    # 创建UI层实例并注入业务逻辑
+    window = WeChatSchedulerUI(scheduler)
+    window.show()
+    
+    sys.exit(app.exec_())
