8ccf214a452638dbd85f9885d02698608306dff1
xie.fangyu <xie.fangyu@byd.com>
2025-04-17 19:15:33 +0800

    优化ui逻辑

--- auto_send/main.py
+++ auto_send/main.py
@@ -14,13 +14,16 @@ from gui import WeChatSchedulerUI
 from scheduler import WeChatScheduler
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    
-    # 创建业务逻辑层实例
-    scheduler = WeChatScheduler()
-    
-    # 创建UI层实例并注入业务逻辑
-    window = WeChatSchedulerUI(scheduler)
-    window.show()
-    
-    sys.exit(app.exec_())
+    try:
+        app = QApplication(sys.argv)
+        
+        # 创建业务逻辑层实例
+        scheduler = WeChatScheduler()
+        
+        # 创建UI层实例并注入业务逻辑
+        window = WeChatSchedulerUI(scheduler)
+        window.show()
+        
+        sys.exit(app.exec_())
+    except Exception as e:
+        sys.exit("Error: {e}")
